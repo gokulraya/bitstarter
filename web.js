f29952908743a691e9071bdddf3eb63ae953d4b6
Gokul Kumar Krishnan <gkrishn6@asu.edu>
2013-07-11 15:18:45 +0530

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs=require('fs');
 var app = express.createServer(express.logger());
-
+var buf,str;
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf=fs.readFileSync('index.html','utf8');
+  //console.log(buf);	
+  var buffer=new Buffer(buf); 
+ str=buffer.toString();
+ response.send(str);
 });
 
 var port = process.env.PORT || 5000;
